8384bffe8a758df1b3f91015af3ff61953e15738
TrueGhxst <Mohamedolad97@gmail.com>
2018-12-05 00:27:34 +0000

    Update views.py

--- views.py
+++ views.py
@@ -21,6 +21,9 @@ from Entertainment_Hub.Data.Content import Mtv
 from Entertainment_Hub.Data.Content import BuzzFeed
 from Entertainment_Hub.Data.Content import Reddit
 
+from Entertainment_Hub.Data.Shows  import TopRatedShows
+
+
 @app.route('/')
 def home():
     """Renders the home page."""   
@@ -50,7 +53,7 @@ def home():
                           
                            
 @app.route('/login')
-def loginPage():
+def login():
     """Renders the home page."""   
 
     return render_template('loginpage.html')
@@ -59,7 +62,7 @@ def loginPage():
 def world():
     """Renders the expanded word page."""   
 
-    
+
 
     return render_template('worldExpand.html', worldexpandeContent =ApiData.Headline_Content, wordldexpandTitle = ApiData.Headline_Title, worldexpandImg = ApiData.Headline_ToImage,
                            worldexpandDesc = ApiData.Headline_Description)
@@ -68,47 +71,62 @@ def world():
 def tech():
     """Renders the expanded tech page."""   
 
-    return render_template('techExpand.html', techexpandContent = Tech.Tech_Content, techexpandTitle = Tech.Tech_Title, techexpandDescripion = Tech.Tech_Description, expandtechImg = Tech.Tech_ToImage)
+    
+    return render_template('techExpand.html', techexpandContent = Tech.Tech_Content, techexpandTitle = Tech.Tech_Title, 
+                           techexpandDescripion = Tech.Tech_Description, expandtechImg = Tech.Tech_ToImage)
 
 @app.route('/sports')
 def sports():
     """Renders the expanded sports page."""   
 
-    return render_template('sportsExpand.html', sportsexpandContent = Sports.Sport_Content, sportsexpandTitle = Sports.Sport_Title, expandsportsDescripion = Sports.Sport_Description, expandtechImg = Sports.Sport_ToImage)
+    return render_template('sportsExpand.html', sportsexpandContent = Sports.Sport_Content, sportsexpandTitle = Sports.Sport_Title,
+                          expandsportsDescripion = Sports.Sport_Description, expandtechImg = Sports.Sport_ToImage)
 
 @app.route('/entertainment')
 def entertainment():
     """Renders the expanded entertainment page."""   
 
-    return render_template('entertainmentExpand.html', entexpandContent = Entertainment.Entertainment_Content, entexpndTitle = Entertainment.Entertainment_Title, expandentDescription = Entertainment.Entertainment_Description,
-                           expandentImg = Entertainment.Entertainment_ToImage)
+    return render_template('entertainmentExpand.html', entexpandContent = Entertainment.Entertainment_Content, entexpndTitle = Entertainment.Entertainment_Title,
+                          expandentDescription = Entertainment.Entertainment_Description, expandentImg = Entertainment.Entertainment_ToImage)
                            
 
 @app.route('/business')
 def business():
     """Renders the expanded business page."""   
 
-    return render_template('businessExpand.html', businessexpandContent = Business.Business_Content, businessexpandTitle = Business.Business_Title, businessexpandDescription = Business.Business_Description,
-                           businessexpandImg = Business.Business_ToImage)
+    return render_template('businessExpand.html', businessexpandContent = Business.Business_Content, 
+                           businessexpandTitle = Business.Business_Title, businessexpandDescription = Business.Business_Description, businessexpandImg = Business.Business_ToImage)
 
 @app.route('/mtv')
 def mtv():
     """Renders the expanded mtv page."""   
 
-    return render_template('mtvExpand.html', mtvexpandContent = Mtv.Mtv_Content, mtvexpadTitle = Mtv.Mtv_Title, mtexpandDescription = Mtv.Mtv_Description, mtvexpandImg = Mtv.Mtv_ToImage)
+    return render_template('mtvExpand.html', mtvexpandContent = Mtv.Mtv_Content, mtvexpadTitle = Mtv.Mtv_Title, mtexpandDescription = Mtv.Mtv_Description, 
+                           mtvexpandImg = Mtv.Mtv_ToImage)
 
 @app.route('/more1')
 def lowerCard_1():
 
     """Renders the expandeds more detail in first lower card"""   
 
-    return render_template('lowerCard1.html',buzzfeedLowerCardContent = BuzzFeed.BuzzFeed_Content, buzzfeedLowerCardTitle = BuzzFeed.BuzzFeed_Title, buzzfeedLowerCardImg = BuzzFeed.BuzzFeed_ToImage,
-                           buzzfeedlowerCardDes = BuzzFeed.BuzzFeed_Description)
+    return render_template('lowerCard1.html',buzzfeedLowerCardContent = BuzzFeed.BuzzFeed_Content, buzzfeedLowerCardTitle = BuzzFeed.BuzzFeed_Title, 
+                           buzzfeedLowerCardImg = BuzzFeed.BuzzFeed_ToImage,  buzzfeedlowerCardDes = BuzzFeed.BuzzFeed_Description)
 
 @app.route('/more2')
 def lowerCard_2():
 
-    """Renders the expandeds more detail in first second card"""   
+    """Renders the expandeds more detail in first lower card"""   
 
-    return render_template('lowerCard2.html', redditLowerCardContent = Reddit.Reddit_Content, redditLowerCardTitle = Reddit.Reddit_Title, redditLowerCardImg = Reddit.Reddit_ToImage,
-                           redditLowerCardDes = Reddit.Reddit_Description)
+    return render_template('lowerCard2.html', redditLowerCardContent = Reddit.Reddit_Content, redditLowerCardTitle = Reddit.Reddit_Title,
+                          redditLowerCardImg = Reddit.Reddit_ToImage, redditLowerCardDes = Reddit.Reddit_Description)
+@app.route('/shows')
+def tv():
+
+    """Renders the expandeds more detail in first lower card"""   
+    
+
+
+    return render_template('showsPage.html',topRated_Name0= TopRatedShows.Shows_0[0], topRated_Overview0 = TopRatedShows.Shows_0[1], topRated_Img0 = TopRatedShows.CompiledImage_0,
+                           topRated_Name1=TopRatedShows.Shows_1[0], topRated_Overview1 = TopRatedShows.Shows_1[1], topRated_Img1=TopRatedShows.CompiledImage_1,
+                           topRated_Name2=TopRatedShows.Shows_2[0], topRated_Overview2 = TopRatedShows.Shows_2[1], topRatedImg2=TopRatedShows.CompiledImage_2,
+                           topRated_Name3=TopRatedShows.Shows_3[0], topRated_Overview3 = TopRatedShows.Shows_3[1], topRatedImg=TopRatedShows.CompiledImage_3)
