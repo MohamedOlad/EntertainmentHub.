e2cf3dd3c129919fa0f4a81de86b3d99bc0b6057
TrueGhxst <Mohamedolad97@gmail.com>
2019-03-23 19:06:06 +0000

    Create viewcontroller2.py

---
+++ viewcontroller2.py
@@ -0,0 +1,10 @@
+"""
+Routes and views for the flask application.
+"""
+from datetime import datetime
+from Entertainment_Hub.Data.Content import ApiData
+from flask import render_template
+from flask import send_file
+from Entertainment_Hub import app
+
+
